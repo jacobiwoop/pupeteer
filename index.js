de3c7ca07a84c7e372f4f22a117eb28f23ec5f17
jacobiwoop <jacobiwoop@gmail.com>
2025-05-26 23:45:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 async function startBrowser() {
     let browser;
